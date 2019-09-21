2ba2295ff63d9faff81fded766707c77a0cf06ed
dedZombie <hemicar92@gmail.com>
2019-09-21 15:24:12 +0200

    Created toggling of left sidebar functionality

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-var navigation  = document.querySelector(".navigation");
-var sidebar     = document.querySelector(".sidebar");
-var content     = document.querySelector(".content");
+var navigation        = document.querySelector(".navigation");
+var sidebar           = document.querySelector(".sidebar");
+var content           = document.querySelector(".content");
+var sidebarToggleBtn  = document.getElementById("toggle-sidebar");
+var contentContainer  = document.querySelector(".content-container");
+var navRight          = document.querySelector(".navigation-right");
 
-var navH = navigation.clientHeight || navigation.offsetHeight
+var navH = navigation.clientHeight || navigation.offsetHeight;
 
 function applyMarginTop(element, value) {
   element.style.marginTop = value + "px";
 }
 
+function clearText() {
+  var list = document.querySelectorAll("ul.list.list-column > li > a");
+  list.forEach(function(item) {
+    item.classList.toggle("d_none");
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   applyMarginTop(content, navH);
+
+  sidebarToggleBtn.addEventListener("click", function() {
+    sidebar.classList.toggle("collapsed");
+    navRight.classList.toggle("ml-40");
+    contentContainer.classList.toggle("ml-40");
+    clearText();
+  });
 });
