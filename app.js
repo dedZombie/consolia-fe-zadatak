9c448ade287625310f8bf0a30b6fd25db91c5007
dedZombie <hemicar92@gmail.com>
2019-09-21 18:33:28 +0200

    Finished app design

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var navigation        = document.querySelector(".navigation");
-var sidebar           = document.querySelector(".sidebar");
+var leftSidebar       = document.getElementById("left-sidebar-menu");
 var content           = document.querySelector(".content");
 var sidebarToggleBtn  = document.getElementById("toggle-sidebar");
 var contentContainer  = document.querySelector(".content-container");
 var navRight          = document.querySelector(".navigation-right");
+var rightSidebar      = document.getElementById("right-sidebar-menu");
+var formToggleBtn     = document.getElementById("toggle-form");
 
 var navH = navigation.clientHeight || navigation.offsetHeight;
 
@@ -18,13 +20,21 @@ function clearText() {
   });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function() {
+  console.log("ready");
   applyMarginTop(content, navH);
+  applyMarginTop(rightSidebar, navH + 3);
 
   sidebarToggleBtn.addEventListener("click", function() {
-    sidebar.classList.toggle("collapsed");
-    navRight.classList.toggle("ml-40");
-    contentContainer.classList.toggle("ml-40");
+    sidebarToggleBtn.children[0].classList.toggle("fa-list");
+    leftSidebar.classList.toggle("collapsed");
+    navRight.classList.toggle("ml-50");
+    contentContainer.classList.toggle("ml-50");
     clearText();
+  });
+
+  formToggleBtn.addEventListener("click", function(){
+    formToggleBtn.children[0].classList.toggle("fa-times");
+    rightSidebar.classList.toggle("visible");
   });
 });
